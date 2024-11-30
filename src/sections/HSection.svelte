9df2ee415a5bd8e5e1bb9bfe93cadefa4705aaa5
Github1234fab <fabien.marceau@gmail.com>
2024-11-30 23:29:12 +0100

    ok

--- src/sections/HSection.svelte
+++ src/sections/HSection.svelte
@@ -52,7 +52,7 @@
 		min-height: 100vh;
 		font-size: 16px;
 		opacity: 1;
-		background-image: url('/creativity.png');
+		background-image: url('/fuseeguyane.png');
 		background-repeat: no-repeat;
 		background-position: top;
 		background-size: cover;
