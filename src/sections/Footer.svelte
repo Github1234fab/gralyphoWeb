6625d02cc3bbf7012ad8c9e9138f86a44f3d7117
Github1234fab <fabien.marceau@gmail.com>
2025-01-06 14:09:25 +0100

    ok

--- src/sections/Footer.svelte
+++ src/sections/Footer.svelte
@@ -12,30 +12,38 @@
 <footer class="footer">
 	<div class="wrapper-contactez">
 		<a href="mailto:gralypho@gmail.com " class="contact" target="_blank"
-			><p>Mail</p><i class="fa-regular fa-envelope icon"></i></a
+			>
+			<i class="fa-regular fa-envelope icon"></i><p>Mail</p></a
 		>
 		<a href="tel:0672901614" class="contact" target="_blank"
-			><p>Tel</p><i class="fa-solid fa-phone-volume icon"></i></a
+			>
+			<i class="fa-solid fa-phone-volume icon"></i><p>Tel</p></a
 		>
 		<a
 			href="https://doodle.com/bp/marceau3/rv-avec-gralypho-agence-de-conception-web"
 			class="contact"
-			target="_blank"><p>RV</p><i class="fa-solid fa-mug-hot icon"></i></a
+			target="_blank"
+			>
+			<i class="fa-solid fa-mug-hot icon"></i><p>RV</p></a
 		>
-		<a href="https://fabienmarceau.com/gralyphoPhotography/index.html" class="contact" target="_blank"
-			><p>Photo</p><i class="fa-solid fa-camera icon"></i></a
+		<a
+			href="https://fabienmarceau.com/gralyphoPhotography/index.html"
+			class="contact"
+			target="_blank"
+			>
+			<i class="fa-solid fa-camera icon"></i><p>Photo</p></a
 		>
 	</div>
 
 	<div class="wrapper-media">
-		<a href="https://www.facebook.com/profile.php?id=61556314663680" target="_blank"
-			><i class="fa-brands fa-square-facebook anim icon"></i></a
+		<a class="reseaux" href="https://www.facebook.com/profile.php?id=61556314663680" target="_blank"
+			><p>Facebook</p><i class="fa-brands fa-square-facebook anim icon "></i></a
 		>
-		<a href="https://twitter.com/home?lang=fr" target="_blank"
-			><i class="fa-brands fa-square-twitter anim icon"></i></a
+		<a class="reseaux"  href="https://twitter.com/home?lang=fr" target="_blank"
+			><p>X</p><i class="fa-brands fa-square-twitter anim icon reseaux"></i></a
 		>
-		<a href="https://www.instagram.com/" target="_blank"
-			><i class="fa-brands fa-square-instagram anim icon"></i></a
+		<a class="reseaux"  href="https://www.instagram.com/" target="_blank"
+			><p>instagram</p><i class="fa-brands fa-square-instagram anim icon reseaux"></i></a
 		>
 		<!-- <a href="https://www.tiktok.com/@gralypho" target="_blank"><i class="fa-brands fa-tiktok anim"></i></a
 		> -->
@@ -78,18 +86,19 @@
 		margin: 0 auto;
 		margin-top: 30px;
 	}
-	
+
 	.wrapper-contactez {
 		display: flex;
 		flex-direction: row;
 		align-items: center;
 		justify-content: center;
 		text-align: center;
-		border-radius: 8px;
+		border-radius: 0px;
 		padding: 40px;
 		gap: 10px;
 		margin-top: 30px;
-		border-top: 1px solid white;
+	
+			margin-bottom: 20px;
 	}
 	.contact {
 		text-decoration: none;
@@ -124,10 +133,9 @@
 		font-size: 1.5rem;
 		margin: 0 0px;
 		transition: 0.4s ease-in-out;
-		
 	}
 	.icon {
-		color: var(--whiteGrey);
+		color: var(--white);
 		font-size: 1.5rem;
 	}
 
@@ -154,5 +162,36 @@
 	.wrapper-media {
 		display: flex;
 		gap: 10px;
+			border-top: 1px dotted  rgb(188, 188, 188);
+			border-bottom: 1px dotted rgb(187, 187, 187);
+			padding: 20px;
+	}
+	.reseaux {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		color: white;
+		gap: 5px;
+	}
+	.reseaux p{
+color: grey;
+font-size: 0.5rem;
+
+	}
+
+	@media screen and (max-width: 768px) {
+		.contact {
+			font-size: 0.6rem;
+			padding: 30px;
+			height: 80px;
+		}
+		.icon{
+			font-size: 1.3rem;
+		}
+			.wrapper-media {
+		display: flex;
+		gap: 20px;
+	}
 	}
 </style>
