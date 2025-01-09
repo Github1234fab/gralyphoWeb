1f8a9c659a8ed1258ac264c8e978778236777c02
Github1234fab <fabien.marceau@gmail.com>
2025-01-09 10:39:42 +0100

    ok

--- src/Components/bannerThree.svelte
+++ src/Components/bannerThree.svelte
@@ -1,53 +1,80 @@
-<div class="container">
-	<div class="bloc imgC">
-		<div class="sub-bloc">
-			<h1 class="title">Augmentez votre potentiel de client.</h1>
+<script>
+	import { onMount } from 'svelte';
 
-			<p>
-				<i class="fa-solid fa-sack-dollar icon-difference"></i>
-				Augmentez vos revenus grâce à une visibilité accrue, grâce à notre expertise SEO. <br /> C'est
-				le moyen d'obtenir plus de visibilité , plus de prospects, plus de clients et plus d'argent!
-			</p>
-			<a href="/referencement-seo-sea" class="lien-bouton">En savoir +</a>
-		</div>
+	onMount(() => {
+		// Applique l'animation à chaque élément avec la classe ".icon"
+
+		gsap.to('.sub-bloc', {
+			opacity: 1, // La valeur cible de l'échelle
+			x: 0,
+			ease: 'power1.inOut',
+			duration: 3,
+			scrollTrigger: {
+				trigger: '.sub-bloc',
+				start: 'top 60%', // Débute lorsque le haut de l'élément atteint 80% du viewport
+				end: 'top 50%', // Termine lorsque le haut de l'élément atteint 50% du viewport
+				scrub: 2
+			}
+		});
+	});
+</script>
+
+<div class="container">
+	<div class="bloc"></div>
+	<div class="sub-bloc">
+		<h1 class="title">Augmentez votre potentiel de client.</h1>
+		<p>
+			<i class="fa-solid fa-sack-dollar icon-difference"></i>
+			Augmentez vos revenus grâce à une visibilité accrue, grâce à notre expertise SEO. <br /> C'est
+			le moyen d'obtenir plus de visibilité , plus de prospects, plus de clients et plus d'argent!
+		</p>
+		<a href="/referencement-seo-sea" class="lien-bouton">En savoir +</a>
 	</div>
 </div>
 
 <style>
 	.container {
-		min-height: auto;
+		height: 800px;
 		display: flex;
-		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		background-color: var(--cta);
 	}
 
 	.bloc {
-		min-height: 90vh;
+		flex: 1;
+		height: 100%;
+		width: 100%;
 		flex-direction: column;
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		padding: 50px;
+		padding: 0px;
 		background-image: url('../Assets/png-gralypho/coiffeur.jpg');
 		background-size: cover;
 		background-position: center;
 		background-repeat: no-repeat;
-		
+
 		/* box-shadow: 0px -10px 20px 2px rgba(0, 0, 0, 0.411); */
 		z-index: 0;
-	position: relative;	
+		position: relative;
 	}
 
 	.sub-bloc {
-		min-height: 90vh;
-		min-width: 100vw;
+		flex: 1;
+		height: 100%;
+		width: 100%;
 		flex-direction: column;
 		display: flex;
 		align-items: center;
 		justify-content: center;
 		z-index: 3;
 		gap: 50px;
+		background: var(--gradient);
+		opacity: 0;
+		transform: translateX(400px);
 	}
-	.bloc::after {
+	/* .bloc::after {
 		content: '';
 		position: absolute;
 		top: 0;
@@ -56,14 +83,7 @@
 		height: 100%;
 		background-color: rgba(0, 0, 0, 0.548);
 		z-index: 2;
-	}
-
-	.imgC {
-		background-image: url('../Assets/png-gralypho/coiffeur.jpg');
-		background-repeat: no-repeat;
-		background-size: cover;
-		background-position: center;
-	}
+	} */
 
 	.title {
 		font-family: var(--Geologica);
@@ -80,7 +100,7 @@
 		/* box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.34); */
 		margin-bottom: 20px;
 	}
-	.bloc p {
+	.sub-bloc p {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
@@ -129,17 +149,12 @@
 	}
 
 	@media screen and (max-width: 768px) {
-		.sub-bloc {
-			margin-top: 170px;
-		}
-
-		.imgC {
-			background-repeat: no-repeat;
-			background-size: cover;
-			background-position: 40% center;
-		}
-		p{
-			max-width: 70%;
+		.container {
+			height: 1300px;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			flex-direction: column;
 		}
 	}
 </style>
--- src/routes/Digital-infos/+page.svelte
+++ src/routes/Digital-infos/+page.svelte
@@ -1,107 +1,108 @@
 <script>
-import Header from '../../sections/Header.svelte';
-import digital from '../../Assets/png-gralypho/23.png';
-  import Mouse from "../../Components/Mouse.svelte";
-  import Button from "../../Components/ButtonCTA.svelte";
-
-
+	import Header from '../../sections/Header.svelte';
+	import digital from '../../Assets/png-gralypho/23.png';
+	import Mouse from '../../Components/Mouse.svelte';
+	import Button from '../../Components/ButtonCTA.svelte';
 </script>
-<Mouse/>
-<Header/>
+
+<Mouse />
+<Header />
 
 <section class="digital-infos">
-        <div class="container">
-      <img src={digital} alt="">
+	<div class="img"></div>
 	<div class="wrapper-digital-infos">
-	
-                <h3>Un site Web, mais pour quoi faire?</h3>
-                	<h1>Profitez de votre outil Digital, en découvrant tout ce qu'il peut vous apporter !</h1>
-                <p>Un site internet c'est en fait un outil très puissant. Un outil que nous allons paramétrer, que nous allons élaborer pour qu'il puisse faire ce que vous souhaitez: <br> Convertir de nouveaux clients, vendre des produits, proposez un service, analyser des marchés, des tendances, gérer vos stocks, vos clients, etc..  <br><br>Votre site internet ou votre application sera en ligne 24/7. Autrement dit, ce sera une hyper présence dont il faut absolument profiter. C'est du temps économisé . Du temps pendant lequel votre site travaille pour vous, vend pour vous, attire de nouveaux clients pour vous, prend des rendez-vous pour vous, présente vos produits pour vous, vos services., etc. <br><br>
-                Déterminer ce que fera votre outil digital est primordial, essentiel. Tout dépend de la stratégie que nous définirons ensemble, pour que votre outil vous permette d'atteindre <span>votre objectif.</span>  </p>
+		<h1>Profitez de votre outil Digital, en découvrant tout ce qu'il peut vous apporter !</h1>
+		<p>
+			Un site internet c'est en fait un outil très puissant. Un outil que nous allons paramétrer,
+			que nous allons élaborer pour qu'il puisse faire ce que vous souhaitez: <br /> Convertir de
+			nouveaux clients, vendre des produits, proposez un service, analyser des marchés, des
+			tendances, gérer vos stocks, vos clients, etc.. <br /><br />Votre site internet ou votre
+			application sera en ligne 24/7. Autrement dit, ce sera une hyper présence dont il faut
+			absolument profiter. C'est du temps économisé . Du temps pendant lequel votre site travaille
+			pour vous, vend pour vous, attire de nouveaux clients pour vous, prend des rendez-vous pour
+			vous, présente vos produits pour vous, vos services., etc. <br /><br />
+			Déterminer ce que fera votre outil digital est primordial, essentiel. Tout dépend de la stratégie
+			que nous définirons ensemble, pour que votre outil vous permette d'atteindre
+			<span>votre objectif.</span>
+		</p>
+		<div class="wrapper-button">
+			<Button />
+		</div>
 	</div>
-        <div class="wrapper-button">
-        <Button/>
-        </div>
-        </div>
 </section>
 
 <style>
+	.digital-infos {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 0px;
+		height: 1200px;
+	}
 
-.digital-infos {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 0px;
-        height: auto; 
-}
-.container{
-        display: flex;
-          justify-content: center;
-        align-items: center;
-        flex-direction: column;
-  
-}
-.container >* {
-        flex-basis: 50%;
-}
-.digital-infos img {
-        width: 70%;
-        height: auto;
-}
-.wrapper-digital-infos {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        gap: 20px;
-        padding: 20px;
-        color: var(--textHero);
-        margin-top: 30px;
-        height: auto;
-        width: 90%;
-}
-.wrapper-digital-infos h1 {
-        font-size: 1.8em;
-        font-weight: 700;
-        text-align: center;
-        font-family: var(--Radio);
-        letter-spacing: -0.5px;
+	.img {
+		flex: 1;
+		height: 100%;
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-image: url('../../Assets/png-gralypho/23.png');
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+	}
+	.wrapper-digital-infos {
+		flex: 1;
+		height: 100%;
+		width: 100%;
+		flex-direction: column;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 50px;
+		background: var(--gradient);
+		opacity: 1;
+		padding: 30px;
+	}
+	.wrapper-digital-infos h1 {
+		font-family: var(--Geologica);
+		font-size: var(--l);
+		color: var(--whiteGrey);
+		font-weight: 900;
+		margin-bottom: 10px;
+		letter-spacing: -1px;
+		text-align: center;
+		line-height: 50px;
+		width: 80%;
+	}
 
-}
-.wrapper-digital-infos h3 {
-        font-family: var(--Radio);
-        font-size: var(--l);
-        font-weight: 700;
-        text-align: center;
-        color: var(--green);
-                margin-top: 15px;
+	.wrapper-digital-infos p {
+		font-family: var(--Poppins);
+		font-size: var(--m);
+		color: var(--whiteGrey);
+		font-weight: 400;
+		letter-spacing: -0.02em;
+		text-align: justify;
+		hyphens: auto;
+		align-self: center;
+		
+	}
+	.wrapper-digital-infos p span {
+		font-weight: 700;
+	}
+	.wrapper-button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-top: 30px;
+		margin-bottom: 50px;
+	}
 
-}
-.wrapper-digital-infos p {
-        font-family: var(--Radio);
-        font-size: var(--m);
-        font-weight: 400;
-        text-align: center;
-        padding: 20px;
-        line-height: 30px;
-}
-.wrapper-digital-infos p span {
-        font-weight: 700;
-}
-.wrapper-button {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-top: 30px;
-        margin-bottom: 50px;
-}
-
-@media screen and (max-width: 768px) {
-        .wrapper-digital-infos {
-                width: 100%;
-        }
-}
-.digital-infos img {
-        width: 100%;
-}
+	@media screen and (max-width: 768px) {
+		.digital-infos {
+			height: 1900px;
+			flex-direction: column;
+		}
+	}
 </style>
