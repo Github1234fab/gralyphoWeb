7d017b0efd8cf5de148ffa87ba76e765598d18a4
Github1234fab <fabien.marceau@gmail.com>
2025-05-07 16:58:50 +0200

    ok

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,22 @@
-import { mdsvex } from 'mdsvex'; // Importation correcte de mdsvex
+// import { mdsvex } from 'mdsvex'; // Importation correcte de mdsvex
 
-import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	},
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+// 		adapter: adapter()
+// 	},
 
-	// Ajout de mdsvex pour pouvoir utiliser Markdown dans tes fichiers .svelte
-	extensions: ['.svelte', '.svx'], // On ajoute les extensions pour que Svelte reconnaisse les fichiers .svx (Markdown)
-	preprocess: mdsvex() // Le préprocesseur mdsvex permet de gérer les fichiers markdown dans Svelte
-};
+// 	// Ajout de mdsvex pour pouvoir utiliser Markdown dans tes fichiers .svelte
+// 	extensions: ['.svelte', '.svx'], // On ajoute les extensions pour que Svelte reconnaisse les fichiers .svx (Markdown)
+// 	preprocess: mdsvex() // Le préprocesseur mdsvex permet de gérer les fichiers markdown dans Svelte
+// };
 
-export default config;
+// export default config;
 
 
 // import { mdsvex } from 'mdsvex';
@@ -82,3 +82,18 @@ export default config;
 // };
 
 // export default config;
+
+import adapter from '@sveltejs/adapter-netlify';
+import { mdsvex } from 'mdsvex';
+
+const config = {
+  extensions: ['.svelte', '.svx'],
+  preprocess: mdsvex(),
+
+  kit: {
+    adapter: adapter(),
+    ssr: true,
+  }
+};
+
+export default config;
