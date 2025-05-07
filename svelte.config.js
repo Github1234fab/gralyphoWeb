525959e915e90ae8b45a9668869d48a0341bd267
Github1234fab <fabien.marceau@gmail.com>
2025-05-07 09:52:19 +0200

    ok

--- svelte.config.js
+++ svelte.config.js
@@ -19,26 +19,22 @@
 // export default config;
 
 
-import { mdsvex } from 'mdsvex'; // Importation correcte de mdsvex
-
+import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.svx'], // <-- doit être à la racine, pas dans `kit`
+	preprocess: mdsvex(),
+
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'index.html' // Important pour gérer les routes profondes
-		  }),
-		  prerender: {
-			entries: ['*'] // pour tout générer
-		  },
-	// Ajout de mdsvex pour pouvoir utiliser Markdown dans tes fichiers .svelte
-	extensions: ['.svelte', '.svx'], // On ajoute les extensions pour que Svelte reconnaisse les fichiers .svx (Markdown)
-	preprocess: mdsvex() // Le préprocesseur mdsvex permet de gérer les fichiers markdown dans Svelte
-}
+			fallback: 'index.html'
+		}),
+		prerender: {
+			entries: ['*']
+		}
+	}
 };
 
 export default config;
