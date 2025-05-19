c444c724511f9bcdf7b75a99315cbe2806bad760
Github1234fab <fabien.marceau@gmail.com>
2025-05-19 15:16:15 +0200

    ok

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,7 +1,12 @@
 <script>
 	export let data;
 	const { article } = data;
-    import Header from "../../../sections/Header.svelte";
+	import Header from '../../../sections/Header.svelte';
+	import LI from '../../../assets/réseaux/LI.png';
+	import WA from '../../../assets/réseaux/WA.png';
+	import FB from '../../../assets/réseaux/FB.png';
+	import TW from '../../../assets/réseaux/TW.png';
+	import mail from '../../../assets/réseaux/mail.png';
 </script>
 
 <svelte:head>
@@ -15,10 +20,32 @@
 </svelte:head>
 
 <Header />
+
 {#if article}
 	<article class="article-content">
+		
+
 		<h1>{article.title}</h1>
 		<p><em>{article.date}</em></p>
+		<section class="share-buttons">
+			<a href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(`https://gralypho.com/blog/${article.slug}`)}`} target="_blank" rel="noopener noreferrer">
+				<img class="icons" src={FB} alt="icone facebook" />
+			</a>
+
+			<a href={`https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(`https://gralypho.com/blog/${article.slug}`)}`} target="_blank" rel="noopener noreferrer">
+				<img class="icons" src={LI} alt="icone Linkedin" />
+			</a>
+
+			<a href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(article.title)}&url=${encodeURIComponent(`https://gralypho.com/blog/${article.slug}`)}`} target="_blank" rel="noopener noreferrer">
+				<img class="icons" src={TW} alt="icone Twitter" />
+			</a>
+
+			<a href={`https://wa.me/?text=${encodeURIComponent(article.title + ' https://gralypho.com/blog/' + article.slug)}`} target="_blank" rel="noopener noreferrer">
+				<img class="icons" src={WA} alt="icone whatsapp" />
+			</a>
+
+			<a href={`mailto:?subject=${encodeURIComponent(article.title)}&body=${encodeURIComponent('Je voulais te partager cet article : https://gralypho.com/blog/' + article.slug)}`} target="_blank" rel="noopener noreferrer"> <img class="icons" src={mail} alt="icone mail" /></a>
+		</section>
 		{#if article.image}
 			<img src={article.image} alt="Illustration pour {article.title}" class="featured-image" />
 		{/if}
@@ -29,6 +56,24 @@
 {/if}
 
 <style>
+	.share-buttons {
+		display: flex;
+		flex-direction: row;
+		align-items: flex-start;
+		justify-content: flex-start;
+		gap: 10px;
+		margin-top: 5px;
+	}
+	.icons {
+		width: 30px;
+		height: 30px;
+		border-radius: 0px;
+		transition: 0.3s ease-in-out;
+	}
+	.icons:hover {
+		transform: scale(1.1);
+		transition: transform 0.2s;
+	}
 	article {
 		font-family: var(--Red);
 		max-width: 800px;
@@ -42,8 +87,9 @@
 	article h1 {
 		font-size: 2rem;
 		margin-bottom: 10px;
+		letter-spacing: -1px;
 	}
-	article p  {
+	article p {
 		font-size: 1rem;
 		color: #555;
 	}
